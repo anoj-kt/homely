bb5609cb25725b00e7a959e3bb7b1904dd0fd690
anoj-kt <anojk28@gmail.com>
2022-09-21 21:54:13 +0200

    imported necessary svg files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import {ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import {ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
+import {ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
+
+function Navbar() {
+    return (
+      <>
+      <h1>Navbar</h1>
+      </>
+    );
+  }
+  
+  export default Navbar;
+  
