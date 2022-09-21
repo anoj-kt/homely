65cb0777052438464e34454237746dbfc1a8555c
anoj-kt <anojk28@gmail.com>
2022-09-21 22:06:32 +0200

    Added css style to navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -19,6 +20,7 @@ function App() {
         <Route path="/sign-up" element={<SignUp/>}></Route>
         <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
       </Routes>
+      <Navbar/>
     </Router>
     </>
   );
