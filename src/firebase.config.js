46575c99d3e1ce1c0033b32def007752e9c0556e
anoj-kt <anojk28@gmail.com>
2022-09-21 20:42:21 +0200

    Added assets folder and installed firebase pkg

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
