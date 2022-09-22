7622831c5d43877cb09e1b42ac4784cce99cadfc
anoj-kt <anojk28@gmail.com>
2022-09-22 09:17:52 +0200

    Added basic form jsx

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -10,8 +10,11 @@ function SignIn() {
     email: "",
     password: ""
   });
+  const { email, password } = formData;
 
   const navigate = useNavigate();
+
+  const onChange = () => {};
 
   return (
     <>
@@ -19,6 +22,27 @@ function SignIn() {
       <header>
         <p className="page__header">Welcome back!</p>
       </header>
+
+      <form>
+        <input 
+          type="email" 
+          className="email__input"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={onChange}
+        />
+        <div className="password__inputDiv">
+          <input 
+            type={showPassword ? "text" : "password"}
+            className="password__input"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+      </form>
     </div>
     </>
   );
