5b5048a21f85d6f07c90595739d0cac5dfae6a1a
anoj-kt <anojk28@gmail.com>
2022-09-22 12:52:11 +0200

    Added login function on sign in comp

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
@@ -20,6 +21,21 @@ function SignIn() {
     }))
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+      if(userCredential.user) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
     <div className="page__container">
@@ -27,7 +43,7 @@ function SignIn() {
         <p className="page__header">Welcome back!</p>
       </header>
 
-      <form>
+      <form onSubmit={onSubmit}>
         <input 
           type="email" 
           className="email__input"
