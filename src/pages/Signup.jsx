b358a1bd330a84a852f30a931cf5efad399b4bff
anoj-kt <anojk28@gmail.com>
2022-09-22 10:40:02 +0200

    Added sign up comp with styling

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,91 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
 function SignUp() {
-    return (
-      <>
-      <h1>Sign Up</h1>
-      </>
-    );
-  }
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData(prev => ({
+      ...prev, [e.target.id]:e.target.value
+    }))
+  };
+
+  return (
+    <>
+    <div className="page__container">
+      <header>
+        <p className="page__header">Welcome back!</p>
+      </header>
+
+      <form>
+        <input 
+          type="text" 
+          className="name__input"
+          placeholder="Name"
+          id="name"
+          value={name}
+          onChange={onChange}
+        />
+        <input 
+          type="email" 
+          className="email__input"
+          placeholder="Email"
+          id="email"
+          value={email}
+          onChange={onChange}
+        />
+        <div className="password__inputDiv">
+          <input 
+            type={showPassword ? "text" : "password"}
+            className="password__input"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={onChange}
+          />
+          <img 
+            src={visibilityIcon} 
+            alt="Show password" 
+            className="showPassword"
+            onClick={() => setShowPassword(prev => !prev)}
+          />
+        </div>
+        <Link 
+          to="/forgot-password"
+          className="forgotPasswordLink"
+        >
+          Forgot Password
+        </Link>
+        <div className="signIn__div">
+          <p className="signIn__text">Sign In</p>
+          <button className="signIn__button">
+            <ArrowRightIcon 
+              fill="#ffffff"
+              width="34px"
+              height="34px"
+            />
+          </button>
+        </div>
+      </form>
+
+      <Link to="/sign-up" className="signUpLink">
+        Sign Up instead
+      </Link>
+    </div>
+    </>
+  );
+}
   
   export default SignUp;
   
