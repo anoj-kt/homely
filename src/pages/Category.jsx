41a14709553c06245cba82f9df9dfbf085ea64bf
anoj-kt <anojk28@gmail.com>
2022-09-23 16:54:20 +0200

    Imports and state initialised

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { collection, getDoc, query, where, orderBy, limit, startAfter } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+import BeatLoader from 'react-spinners/BeatLoader';
+
+import { db } from '../firebase.config';
 
 function Category() {
-  return (
-    <div>Category</div>
-  )
+    const [listings, setListings] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const params = useParams();
+
+    useEffect(() => {
+        const fetchListings = async () => {
+            try {
+                
+            } catch (error) {
+                
+            }
+        }
+    })
+
+    return (
+        <div>Category</div>
+    )
 }
 
 export default Category
