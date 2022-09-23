69d0974e3be88ffd57e3b2ad069e76ff800e375b
anoj-kt <anojk28@gmail.com>
2022-09-23 14:46:50 +0200

    Used UseRef to remove error

--- src/hooks/UseAuthStatus.js
+++ src/hooks/UseAuthStatus.js
@@ -1,19 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false);
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true);
+    const isMounted = useRef(true)
 
     useEffect(() => {
-        const auth = getAuth()
-        onAuthStateChanged(auth, (user) => {
-            if(user) {
-                setLoggedIn(true)
-            }
-            setIsLoading(false)
-        })
-    })
+        if(isMounted) {
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user) => {
+                if(user) {
+                    setLoggedIn(true)
+                }
+                setIsLoading(false)
+            })
+        }
+        
+        return () => {
+            isMounted.current = false
+        }
+    }, [isMounted])
 
     return { loggedIn, isLoading }
 }
