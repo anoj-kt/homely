6e0cecf63594f287fb0a54abb40312b2db4be6cb
anoj-kt <anojk28@gmail.com>
2022-09-23 16:01:02 +0200

    Added google auth func

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,17 +1,38 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 
 import { db } from '../firebase.config'
 import googleIcon from '../assets/svg/googleIcon.svg'
+import { calculateNewValue } from '@testing-library/user-event/dist/utils';
 
 function OAuth () {
     const navigate = useNavigate();
     const location = useLocation();
 
-    const onGoogleClick = () => {
+    const onGoogleClick = async () => {
+        try {
+            const auth = getAuth()
+            const provider = new GoogleAuthProvider()
+            const result = await signInWithPopup(auth, provider)
+            const user = result.user
 
+            const userRef = doc(db, 'users', user.uid)
+            const selectedDoc = await getDoc(userRef)
+
+            if(!selectedDoc.exists()) {
+                await setDoc(doc(db, 'users', user.uid), {
+                    name: user.displayName,
+                    email: user.email,
+                    timestampe: serverTimestamp()
+                })
+            }
+
+            navigate('/')
+        } catch (error) {
+            toast.error('Google authorization failed!')
+        }
     }
 
     return (
