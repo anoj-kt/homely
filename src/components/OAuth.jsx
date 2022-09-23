142bc55d711ea2df0c65d2b5473726f4f6011098
anoj-kt <anojk28@gmail.com>
2022-09-23 15:36:16 +0200

    Necessary imports

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,3 +1,10 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+
+import { db } from '../firebase.config'
+import googleIcon from '../assets/svg/googleIcon.svg'
 
 function OAuth () {
     return (
