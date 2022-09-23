814b3932721e0842bb35c263868b0305baba2c4a
anoj-kt <anojk28@gmail.com>
2022-09-23 16:18:07 +0200

    code check on app js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 import Navbar from './components/Navbar';
-import Explore from './pages/Explore'
-import Offers from './pages/Offers'
-import Profile from './pages/Profile'
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
 import PrivateRoute from './components/PrivateRoutes';
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import ForgotPassword from './pages/ForgotPassword'
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
