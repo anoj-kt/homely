881a49ade5031058746ce986d53cc93262bfb46d
anoj-kt <anojk28@gmail.com>
2022-09-23 14:57:43 +0200

    Added spinner to private route comp

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,11 +1,13 @@
 import { Navigate, Outlet } from 'react-router-dom';
-import useAuthStatus from '../hooks/UseAuthStatus'
+import BeatLoader from "react-spinners/BeatLoader";
+
+import useAuthStatus from '../hooks/UseAuthStatus';
 
 const PrivateRoute = () => {
     const { loggedIn, isLoading } = useAuthStatus();
 
     if(isLoading) {
-        return <h3>Loading</h3>
+        return <BeatLoader color="#00cc66" />
     }
 
     return loggedIn? <Outlet/> : <Navigate to="/sign-in" />
