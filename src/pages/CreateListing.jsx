53d670c0b4e844d8d699b68302944a89c6129260
anoj-kt <anojk28@gmail.com>
2022-09-29 13:59:44 +0200

    Created initial state for formdata

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
 
 function CreateListing() {
+    const [formData, setFormData] = useState({
+        type: 'rent',
+        name: '',
+        bedrooms: 1,
+        bathrooms: 1,
+        parking: false,
+        furnished: false,
+        address: '',
+        offerr: false,
+        regularPrice: 0,
+        discountedPrice: 0,
+        images: {},
+        latitude: 0,
+        longitude: 0
+    })
+
   return (
     <div>CreateListing</div>
   )
