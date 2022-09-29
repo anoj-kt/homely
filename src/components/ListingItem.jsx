70cc1bbe539928d25bea0f3eb53e1a3ca0d23773
anoj-kt <anojk28@gmail.com>
2022-09-29 13:00:14 +0200

    Added listing name, location and price

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -10,6 +10,11 @@ function ListingItem({listing, id}) {
     <li className="category__listing">
         <Link to={`/category/${listing.type}/${id}`} className="category__listingLink">
             <img src={listing.imageUrls[0]} alt={listing.name} className="category__listingImg" />
+            <p className="category__listingLocation">{listing.location}</p>
+            <p className="category__listingName">{listing.name}</p>
+            <p className="category__listingPrice">
+                {listing.offer ? listing.discountedPrice : listing.regularPrice} €
+            </p>
         </Link>
     </li>
   )
