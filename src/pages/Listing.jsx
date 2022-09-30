3bb86db58a276f4b0587dc6169cb0a72b2071f23
anoj-kt <anojk28@gmail.com>
2022-09-30 15:13:48 +0200

    Initial state and variables

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,6 +8,14 @@ import { db } from '../firebase.config';
 import shareIcon from '../assets/svg/shareIcon.svg'
 
 function Listing() {
+    const [listing, setListing] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const [shareLinkCopied, setShareLinkCopied] = useState(null)
+
+    const navigate = useNavigate()
+    const params = useParams()
+    const auth = getAuth()
+
   return (
     <div>Listing</div>
   )
