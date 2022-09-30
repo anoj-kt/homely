3eb9ea853ae96526df29eecfadd912b59da9fe85
anoj-kt <anojk28@gmail.com>
2022-09-30 13:50:57 +0200

    Completed func to store images

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -95,6 +95,47 @@ function CreateListing() {
             location = address
         }
 
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage()
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+                const storageRef = ref(storage, 'images/' + fileName)
+                const uploadTask = uploadBytesResumable(storageRef, image)
+
+                uploadTask.on('state_changed', 
+                    (snapshot) => {
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log('Upload is ' + progress + '% done');
+                        switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                        }
+                    }, 
+                    (error) => {
+                        reject(error)
+                    }, 
+                    () => {
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        resolve(downloadURL);
+                        });
+                    }
+                    );
+                })
+        }
+
+        const imageUrls = await Promise.all(
+            [...images].map(img => storeImage(img))
+        ).catch(() => {
+            setIsLoading(false)
+            toast.error('Images could not be uploaded')
+            return
+        })
+
         setIsLoading(false)
     }
 
