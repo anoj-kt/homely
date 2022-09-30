59a5bdf64ede817d687674bb2ec293c2133d87ef
anoj-kt <anojk28@gmail.com>
2022-09-30 15:11:24 +0200

    Initial imports

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { getDoc, doc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { BeatLoader } from 'react-spinners/BeatLoader';
+
+import { db } from '../firebase.config';
+import shareIcon from '../assets/svg/shareIcon.svg'
 
 function Listing() {
   return (
