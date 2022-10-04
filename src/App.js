fd81953a6596c5b148a124719d821a449bb08327
anoj-kt <anojk28@gmail.com>
2022-10-04 12:35:58 +0200

    Created and imported contact page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
         <Route path="/create-listing" element={<CreateListing/>}/>
         <Route path="/category/:categoryName/:listingId" element={<Listing/>}/>
+        <Route path="/contact/:ownerId" element={<Contact/>}/>
       </Routes>
       <Navbar/>
     </Router>
