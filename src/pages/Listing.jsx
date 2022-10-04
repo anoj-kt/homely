7f95e0ae384869e03b367d17399a0e5544929c79
anoj-kt <anojk28@gmail.com>
2022-10-04 13:40:58 +0200

    Necessary imports

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { async } from '@firebase/util';
+import { connectStorageEmulator } from 'firebase/storage';
 import BeatLoader from 'react-spinners/BeatLoader';
 
 import { db } from '../firebase.config';
 import shareIcon from '../assets/svg/shareIcon.svg'
-import { async } from '@firebase/util';
-import { connectStorageEmulator } from 'firebase/storage';
 
 function Listing() {
     const [listing, setListing] = useState(null)
