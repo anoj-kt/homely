6dc63a52c4da5f827aef92dd8d53280194c20b85
anoj-kt <anojk28@gmail.com>
2022-10-04 12:38:43 +0200

    Necessary imports in Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+
+import { db } from '../firebase.config';
 
 function Contact() {
   return (
