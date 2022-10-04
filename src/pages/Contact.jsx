a3aa402b242f8dc242eaaac288933de2e3f81946
anoj-kt <anojk28@gmail.com>
2022-10-04 12:43:24 +0200

    Initialised stated and useeffect to fetch data

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,6 +6,28 @@ import { toast } from 'react-toastify';
 import { db } from '../firebase.config';
 
 function Contact() {
+    const [message, setMessage] = useState('');
+    const [owner, setOwner] = useState(null);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const params = useParams();
+
+    useEffect(() => {
+        const getOwner = async () => {
+            const docRef = doc(db, 'users', params.ownerId)
+            const selectedDoc = await getDoc(docRef)
+
+            if(selectedDoc.exists()) {
+                setOwner(selectedDoc.data())
+            } else {
+                toast.error('Could not get owner details!')
+            }
+        }
+
+        getOwner()
+
+    }, [params.ownerId])
+
   return (
     <div>Contact</div>
   )
