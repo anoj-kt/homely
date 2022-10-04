e0497e31e5d5596cd7b9f63bf546fd06ca1a4712
anoj-kt <anojk28@gmail.com>
2022-10-04 13:31:01 +0200

    Added leaflet map with styles

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 import { getDoc, doc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import BeatLoader from 'react-spinners/BeatLoader';
@@ -80,6 +80,23 @@ function Listing() {
             </ul>
 
             <p className="listing__location-title">Location</p>
+
+            <div className="leaflet__container">
+                    <MapContainer 
+                        style={{height: "100%", width: "100%"}} 
+                        center={[listing.geolocation.lat, listing.geolocation.lng]}
+                        zoom={13}
+                        scrollWheelZoom={false}
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                            <Popup>{listing.location}</Popup>
+                        </Marker>
+                    </ MapContainer>
+            </div>
 
             {auth.currentUser?.uid !== listing.userRef && (
                 <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className="primaryButton">Contact Owner</Link>
