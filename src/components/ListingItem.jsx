bd45fb570fb4ed4e647f8aa531bcdbea3e8791ef
anoj-kt <anojk28@gmail.com>
2022-10-05 13:42:53 +0200

    Added onEdit to ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as DeleteIcon} from '../assets/svg/deleteIcon.svg';
+import { ReactComponent as EditIcon} from '../assets/svg/editIcon.svg';
 import bedIcon from '../assets/svg/bedIcon.svg';
 import bathtubIcon from '../assets/svg/bathtubIcon.svg';
 
 
-function ListingItem({listing, id, onDelete}) {
+function ListingItem({listing, id, onEdit, onDelete}) {
   return (
     <li className="category__listing">
         <Link to={`/category/${listing.type}/${id}`} className="category__listingLink">
@@ -45,6 +46,14 @@ function ListingItem({listing, id, onDelete}) {
                 fill="red" 
                 onClick={() => onDelete(listing.id, listing.name)} 
             />
+        )}
+
+        {onEdit && (
+            <EditIcon 
+            className="icon__edit" 
+            fill="red" 
+            onClick={() => onEdit(id)} 
+        />
         )}
     </li>
   )
