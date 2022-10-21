7227291f3ddbf446625c8a8d9cc307ec2758f598
anoj-kt <anojk28@gmail.com>
2022-10-21 13:25:27 +0200

    Flex grow on category links

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -14,21 +14,22 @@ function Explore() {
         <main>
 
           <Slider/>
-
-          <div className="explore__categories">
-            <Link to="/category/rent" className="explore__category">
-              <img src={rentIcon} alt="Properties for rent" className="explore__category-img"/>
-              <p className="explore__category-name">Rent</p>
-            </Link>
-            <Link to="/category/sale" className="explore__category">
-              <img src={saleIcon} alt="Properties for sale" className="explore__category-img"/>
-              <p className="explore__category-name">Buy</p>
+          <div className="explore__categories-container">
+            <div className="explore__categories">
+              <Link to="/category/rent" className="explore__category">
+                <img src={rentIcon} alt="Properties for rent" className="explore__category-img"/>
+                <p className="explore__category-name">Rent</p>
+              </Link>
+              <Link to="/category/sale" className="explore__category">
+                <img src={saleIcon} alt="Properties for sale" className="explore__category-img"/>
+                <p className="explore__category-name">Buy</p>
+              </Link>
+            </div>
+            <Link to="/create-listing" className="explore__category-post">
+              <img src={postPropertyIcon} alt="Properties for sale" className="explore__category-img"/>
+              <p className="explore__category-name">List your property</p>
             </Link>
           </div>
-          <Link to="/create-listing" className="explore__category-post">
-            <img src={postPropertyIcon} alt="Properties for sale" className="explore__category-img"/>
-            <p className="explore__category-name">List your property</p>
-          </Link>
         </main>
       </div>
     );
