46af342b246a4cf2186ef5ae08a557049a4f91de
anoj-kt <anojk28@gmail.com>
2022-10-31 09:39:07 +0100

    Import icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import globeIcon from '../assets/svg/globe.svg';
+import linkedInIcon from '../assets/svg/linkedin.svg';
+import githubIcon from '../assets/svg/github.svg';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        <p>Developed by Anoj</p>
+        <div>
+            <img src={globeIcon} alt="Anoj website" className="footer__icon" />
+            <img src={linkedInIcon} alt="Anoj website" className="footer__icon" />
+            <img src={githubIcon} alt="Anoj website" className="footer__icon" />
+        </div>
+    </div>
   )
 }
 
